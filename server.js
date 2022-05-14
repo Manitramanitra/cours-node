70b9155428c49f6e3c2d437396f760dadd68d110
Manitra Randriamanantena <randiluc@gmail.com>
2022-05-14 08:54:23 +0300

    création d'un serveur

--- server.js
+++ server.js
@@ -1,9 +1,50 @@
-const http = require("http");
+let http = require("http");
+let fs = require("fs");
+let url = require("url");
+const EventEmiter = require("events");
 
-let server = http.createServer();
-server.on("request", (request, response) => {
-  response.writeHead(200);
-  response.end("salut comment ça va?");
+let App = {
+  start: (port) => {
+    let emitter = new EventEmiter();
+    let server = http
+      .createServer((request, response) => {
+        response.writeHead(200, {
+          "Content-type": "text/html; charset=utf-8",
+        });
+        if (request.url === "/") {
+          emitter.emit("root", response);
+        }
+        response.end();
+      })
+      .listen(port);
+    return emitter;
+  },
+};
+
+let app = App.start(8080);
+app.on("root", (response) => {
+  // s'execute lorsque la page d'accueil est démarrée
+  response.write("Je suis à la racine");
 });
 
-server.listen(8080);
+/*let server = http.createServer();
+server.on("request", (request, response) => {
+  response.writeHead(200);
+  let query = url.parse(request.url, true).query;
+  let name = query.name === undefined ? "anonyme" : query.name;
+
+  fs.readFile("index.html", "utf-8", (err, data) => {
+    if (err) {
+      response.writeHead(404);
+      response.end("ce fichier n'existe pas");
+    } else {
+      response.writeHead(200, {
+        "Content-type": "text/html; charset=utf-8",
+      });
+      data = data.replace("{{name}}", name);
+      response.end(data);
+    }
+  });
+});
+
+server.listen(8080);*/
